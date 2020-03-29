495fdb1eac6c8023a51cd9302d0884c58fc78c5c
jafojial <alex.jaza@yahoo.fr>
2020-03-29 17:14:00 +0100

    Initial commit

---
+++ accounts/views.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+from .forms import SignUpForm
+
+# Create your views here.
+
+def signup(request):
+    # return render(request, 'signup.html')
+
+    # form = UserCreationForm()
+    # return render(request, 'signup.html', {'form': form})
+
+    if request.method == 'POST':
+        # form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        # form = UserCreationForm()
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
